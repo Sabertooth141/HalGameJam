a3c096cdf95607b398f698bd68a457b09329f385
zweihanzance297167 <zwei.hanzance297167@gmail.com>
2026-07-28 14:00:52 +0900

    ステージ選択→シーン遷移の処理を作った

--- Assets/Scripts/SceneTransition/StageSelect.cs
+++ Assets/Scripts/SceneTransition/StageSelect.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class StageSelect : MonoBehaviour
 {
     //----------------------------
+    // 列挙型
+    //----------------------------
+    //シーン遷移のモード
+    public enum TransitionMode
+    {
+        NoEffect = 0, //特殊効果なしの即時切り替わり
+        FadeOut = 1, //フェードアウト
+        SlideIn = 2, //スライドイン
+    }
+
+    //----------------------------
     // パラメータ
     //----------------------------
     [Header("ステージ選択星の半径")]
     [Tooltip("ステージ選択星の半径を設定する")]
     [Min(0f)]
     [SerializeField] private float radius = 1f; // 半径
+
+    [Header("シーン遷移のモード")]
+    [Tooltip("モード選択（特殊効果無・フェード・スライド）")]
+    [SerializeField] private TransitionMode transitionMode = TransitionMode.SlideIn;
 
     [Header("次のシーン")]
     [Tooltip("ロケットが当たったときに遷移するシーンを設定")]
@@ -36,7 +51,7 @@ public class StageSelect : MonoBehaviour
 
     void FixedUpdate()
     {
-        //ロケットの位置を取得してゴール判定を行う
+        //ロケットの位置を取得して接触判定を行う
         if (rocketController == null) return;
         Vector3 pos = rocketController.transform.position;
         Vector2 rocketPos = new Vector2(pos.x, pos.y);
@@ -54,10 +69,23 @@ public class StageSelect : MonoBehaviour
     {
         Vector2 goalPos = new Vector2(transform.position.x, transform.position.y);
         float distance = Vector2.Distance(rocketPos, goalPos);
-        if (distance < radius) // ゴールの半径を使用
+        if (distance < radius) // 半径を使用
         {
             Debug.Log("Goal!");
-            SceneTransitioner.Instance.LoadSceneInstant(SceneTransitioner.SceneName.SampleScene1);
+            switch (transitionMode)
+            {
+                case TransitionMode.NoEffect:
+                    SceneTransitioner.Instance.LoadSceneInstant(nextScene);
+                    break;
+                case TransitionMode.FadeOut:
+                    SceneTransitioner.Instance.LoadSceneFade(nextScene);
+                    break;
+                case TransitionMode.SlideIn:
+                    SceneTransitioner.Instance.LoadSceneSlide(nextScene);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
