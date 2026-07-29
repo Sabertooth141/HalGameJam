57a3a8cccde939d61307de6ec9efbf1bd29d6f33
Zihao Ding <44132181+Sabertooth141@users.noreply.github.com>
2026-07-29 13:38:29 +0900

    SFX音量調整

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -126,7 +126,15 @@ public class GameController : MonoBehaviour
     {
         if (kb.escapeKey.wasPressedThisFrame)
         {
-            SceneTransitioner.Instance.LoadSceneSlide(SceneTransitioner.SceneName.Title);
+            SoundManager.Instance.Play("Confirm");
+
+            if (SceneTransitioner.Instance.GetCurrentScene(SceneTransitioner.SceneName.StageSelect))
+            {
+                SceneTransitioner.Instance.LoadSceneSlide(SceneTransitioner.SceneName.Title);
+                return;
+            }
+
+            SceneTransitioner.Instance.LoadSceneSlide(SceneTransitioner.SceneName.StageSelect);
         }
     }
 
--- Assets/Scripts/UI/EscapeController.cs
+++ Assets/Scripts/UI/EscapeController.cs
@@ -4,6 +4,14 @@ public class EscapeController : MonoBehaviour
 {
     public void OnEscapePressed()
     {
+        SoundManager.Instance.Play("Confirm");
+
+        if (SceneTransitioner.Instance.GetCurrentScene(SceneTransitioner.SceneName.StageSelect))
+        {
+            SceneTransitioner.Instance.LoadSceneSlide(SceneTransitioner.SceneName.Title);
+            return;
+        }
+
         SceneTransitioner.Instance.LoadSceneSlide(SceneTransitioner.SceneName.StageSelect);
     }
 }
