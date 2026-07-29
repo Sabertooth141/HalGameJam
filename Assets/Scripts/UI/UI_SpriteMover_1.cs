5048c16d910046b7a28e341ce911144f23f292d7
zweihanzance297167 <zwei.hanzance297167@gmail.com>
2026-07-29 10:56:51 +0900

    StageSelect画面にスプライトを表示

---
+++ Assets/Scripts/UI/UI_SpriteMover_1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UI_SpriteMover_1 : MonoBehaviour
+{
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
